40063160bc2ddb546bede4e829d396ec605f4c3e
Samuel C <samuel-calderon--xd@hotmail.com>
2022-05-12 23:41:55 -0700

    Update cinco pantallas de registros

--- proveedoresCliente/proveedoresCliente/Factura.cs
+++ proveedoresCliente/proveedoresCliente/Factura.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace proveedoresCliente
 {
-    public partial class Form1 : Form
+    public partial class Factura : Form
     {
-        public Form1()
+        public Factura()
         {
             InitializeComponent();
         }
