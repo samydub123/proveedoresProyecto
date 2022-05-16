0e7e9f8b20dbbd7c0700f7888407edcea23d0f48
Juan Torres <juan.francisco.torres.valenzuela@uabc.edu.mx>
2022-05-16 10:57:57 -0700

    Nuevos menus

---
+++ proveedoresCliente/proveedoresCliente/Animales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proveedoresCliente
+{
+    public partial class Animales : Form
+    {
+        public Animales()
+        {
+            InitializeComponent();
+        }
+    }
+}
